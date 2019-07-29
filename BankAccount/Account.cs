65c0770bdc6d100053c27807e394c200d7738109
Dennis Kafton <boom1axe@att.net>
2019-07-29 15:01:24 -0700

    Added Class Account

---
+++ BankAccount/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Repersents a customer bank account
+    /// </summary>
+    public class Account
+    {
+        // name of account holder
+        // account number
+        // balance
+        public string AccountNumber { get; set; }
+
+        public string Owners { get; set; }
+
+        public double Balance { get; set; }
+
+        /// <summary>
+        /// Adds an amount to the current balance
+        /// and returns the updated balance
+        /// </summary>
+        /// <param name="amt"></param>
+        /// <returns></returns>
+        public double Deposit(double amt)
+        {
+            Balance += amt;
+            return Balance;
+            throw new NotImplementedException();
+        }
+
+        public double Withdraw(double amt)
+        {
+            Balance -= amt;
+            return Balance;
+            throw new NotImplementedException();
+        }
+    }
+}
